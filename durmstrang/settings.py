7a0521ab7779878c858800fae5146f66b23f7287
cuzik <cuzikjack@gmail.com>
2018-09-15 11:56:50 -0300

    fix some minor and run black

--- durmstrang/settings.py
+++ durmstrang/settings.py
@@ -135,9 +135,9 @@ django_heroku.settings(locals())
 from django.contrib.messages import constants as messages
 
 MESSAGE_TAGS = {
-    messages.DEBUG: 'alert-info',
-    messages.INFO: 'alert-info',
-    messages.SUCCESS: 'alert-success',
-    messages.WARNING: 'alert-warning',
-    messages.ERROR: 'alert-danger',
+    messages.DEBUG: "alert-info",
+    messages.INFO: "alert-info",
+    messages.SUCCESS: "alert-success",
+    messages.WARNING: "alert-warning",
+    messages.ERROR: "alert-danger",
 }
